3282f18bf9e511076ae9e1bc648d064c3e711f3c
Dorian <dorian.mairybe@gmail.com>
2022-10-13 16:46:36 +0200

    init react projects

---
+++ client/src/components/Navbar.tsx
@@ -0,0 +1,24 @@
+// dependencies
+import { Link } from 'react-router-dom' 
+import "../scss/navbar.scss";
+
+const Navbar = () => {
+
+    return (
+        <header>
+            <div className="container">
+                <Link to="/">
+                    <h1>Project Manager</h1>
+                </Link>
+                <Link to="/register">
+                    <h5>Register</h5>
+                </Link>
+                <Link to="/login">
+                    <h5>Login</h5>
+                </Link>
+            </div>
+        </header>
+    )
+}
+
+export default Navbar
---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react"
+
+const Home = () => {
+
+    const [projects, setProjects] = useState(null);
+
+    useEffect(() => {
+        const fetchProjects = async  () => {
+            const response = await fetch('http://localhost:5000/api/projects')
+            const json = await response.json()
+
+            if (response.ok) {
+                setProjects(json)
+            }
+        }
+
+        fetchProjects()
+    }, []) 
+
+    return (
+        <div className="home">
+            <div className="projects">
+                {projects && projects.map((project: any) => (
+                    <p key={project._id}>{project.title}</p>
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default Home
