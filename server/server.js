161fa05d988ccf278f2a2a8d372d9000f2d248ab
Dorian <dorian.mairybe@gmail.com>
2022-10-13 10:43:32 +0200

    fix routes projects

--- server/server.js
+++ server/server.js
@@ -1,32 +1,42 @@
 // dependencies
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-require('dotenv').config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const router = express.Router();
+require("dotenv").config();
 
+//routes dependencies
+const projectsRoutes = require("./routes/projects_router");
 
 // server configuration
 const app = express();
 const port = process.env.PORT || 5000;
-
-
-// midleware
 app.use(cors());
-app.use(express.json());
 app.use(bodyParser.json());
 
+// midleware
+router.use(function timeLog(req, res, next) {
+  console.log("Time: ", Date.now());
+  next();
+});
+
+//models import
+
+// routes
+app.use("/api/projects", projectsRoutes);
 
 // Database configuration
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri);
 const connection = mongoose.connection;
-connection.once('open', () => {
-console.log("MongoDB database connection established successfully");
-})
-
+connection.once("open", () => {
+  console.log("MongoDB database connection established successfully");
+});
 
 // app configuration
+app.use(express.json());
+
 app.listen(port, () => {
-console.log(`Server is running on port: ${port}`);
+  console.log(`Server is running on port: ${port}`);
 });
